656d89271b187aee4d01de810f8be0b21fd1768d
qwertyabu <monkey.famil4@gmail.com>
2025-02-17 22:30:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let admin = prompt('Nechta Qiyqiriq сет')
+alert(admin)
+let one = 45000 * (`${admin}`)
+let user = confirm(`${admin} qiyqirik jami narhi ${one} 
+    mahsulotni zakaz kilasmi
+`)
+alert(user)
+let two =('zakaz kabul kilindi')
+
+alert(two)
